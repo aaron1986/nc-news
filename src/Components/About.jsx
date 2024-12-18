847ba73d1d629e4ed454cec0ba410aa18ac52f63
aaron1986 <aaronbrucesmith86@gmail.com>
2024-12-18 18:58:04 +0000

    fixed the voting function

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,8 +6,9 @@ export default function About() {
        <p>I am enamored to be part of the exceptional experience provided by the Northcoders bootcamp. 
         This program has been instrumental in my journey toward becoming a junior React developer. 
         It has not only boosted my confidence, but also significantly enhanced my technical skills.<br /><br /> 
-        I'm currently working on a REACT application, the apps' main purpose is to retrieve articles 
-        from a database which I created using PostgreSQL, and displaying the articles using an API.  
+        I'm currently working on a REACT application to retrieve articles from a database which I created using PostgreSQL 
+        in back end development. Moreover, the user can click on and read each individual article 
+        (each article contains comments and an option to vote on the article). 
         </p>     
     </section>
         </>
